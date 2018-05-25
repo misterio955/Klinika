b23df3fdcedea24fd3271c9f6519bf81768d46fd
Exatros <exatros@gmail.com>
2018-05-25 15:50:00 +0200

    Podłączenie przyciskow z baza. Walidacja danych. Dodanie okienka allert. Zakladka "Lista pacjentow"

--- Clinic/src/clinic/RegisterWindowController.java
+++ Clinic/src/clinic/RegisterWindowController.java
@@ -32,7 +32,7 @@ public class RegisterWindowController implements Initializable {
             dbConn.setVisitsList();
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(RegisterWindowController.class.getName()).log(Level.SEVERE, null, ex);
-       }
+        }
     }
 
     //    REGISTER
@@ -74,38 +74,88 @@ public class RegisterWindowController implements Initializable {
     @FXML
     private TableView<Patient> tableListP;
     @FXML
-    private TableColumn<Patient,String> columnTablePeselP;
+    private TableColumn<Patient, String> columnTablePeselP;
     @FXML
-    private TableColumn<Patient,String> columnTableNameP;
+    private TableColumn<Patient, String> columnTableNameP;
     @FXML
-    private TableColumn<Patient,String> columnTableSurnameP;
+    private TableColumn<Patient, String> columnTableSurnameP;
     @FXML
-    private TableColumn<Patient,String> columnTableEmailP;
-    @FXML
-    private TableColumn<Patient,String> columnTableIDP;
+    private TableColumn<Patient, String> columnTableEmailP;
 
     @FXML
     private void findPeselP(ActionEvent event) {
-        List<Patient> x = dbConn.getPatientsList();
-        for(Patient patient: x)
-        {
-//            System.out.println(patient.getPesel());
-            tableListP.getItems().add(new Patient(patient.getID(),patient.getPesel(),patient.getImie(),patient.getNazwisko(),patient.getTelefon()));
+        tableListP.getItems().clear();
+
+        boolean isNumbers = false;
+        char[] isNumbersOnly = textFindPeselP.getText().toCharArray();
+
+        //Sprawdzanie czy pesel posiada same liczby
+        for (int i = 0; i < isNumbersOnly.length; i++) {
+            if (isNumbersOnly[i] >= '0' && isNumbersOnly[i] <= '9') {
+                isNumbers = true;
+            } else {
+                isNumbers = false;
+                break;
+            }
         }
 
-        columnTableIDP.setCellValueFactory(new PropertyValueFactory<>("ID"));
-        columnTableIDP.setVisible(false);
-        columnTablePeselP.setCellValueFactory(new PropertyValueFactory<>("Pesel"));
-        columnTableNameP.setCellValueFactory(new PropertyValueFactory<>("Imie"));
-        columnTableSurnameP.setCellValueFactory(new PropertyValueFactory<>("Nazwisko"));
-        columnTableEmailP.setCellValueFactory(new PropertyValueFactory<>("Telefon"));
+        //Sprawdzenie czy pesel ma 11 znakow i czy jest w bazie
+        if (textFindPeselP.getLength() == 11 && dbConn.getPatientByPESEL(textFindPeselP.getText()) != null && isNumbers == true) {
+            Patient patient = dbConn.getPatientByPESEL(textFindPeselP.getText());
+
+            tableListP.getItems().add(new Patient(patient.getID(), patient.getPesel(), patient.getImie(), patient.getNazwisko(), patient.getTelefon()));
+
+            columnTablePeselP.setCellValueFactory(new PropertyValueFactory<>("Pesel"));
+            columnTableNameP.setCellValueFactory(new PropertyValueFactory<>("Imie"));
+            columnTableSurnameP.setCellValueFactory(new PropertyValueFactory<>("Nazwisko"));
+            columnTableEmailP.setCellValueFactory(new PropertyValueFactory<>("Telefon"));
+        } else {
+            alert.setTitle("Nieprawidłowa długość peselu");
+            alert.setHeaderText("Błąd w polu pesel");
+            alert.setContentText("Podany pesel jest błędny lub nie ma takiego pacjenta w bazie");
+            alert.showAndWait();
+
+        }
 
     }
 
     @FXML
     private void findInicialP(ActionEvent event) {
+        tableListP.getItems().clear();
+
+        String Help = textFindNameP.getText().toLowerCase() + textFindSurnameP.getText().toLowerCase();
+        char[] isLetterOnly = Help.toCharArray();
+        boolean isLetter = false;
+        //Sprawdzanie czy imie i nazwisko posiada same litery
+        for (int i = 0; i < isLetterOnly.length; i++) {
+            if (isLetterOnly[i] >= 'a' && isLetterOnly[i] <= 'z') {
+                isLetter = true;
+            } else {
+                isLetter = false;
+                break;
+            }
+        }
+
+        if (dbConn.getPatientByName(textFindNameP.getText(), textFindSurnameP.getText()) != null && isLetter == true) {
+            List<Patient> patient = dbConn.getPatientByName(textFindNameP.getText(), textFindSurnameP.getText());
+
+            for (Patient patientTab : patient) {
+                tableListP.getItems().add(new Patient(patientTab.getID(), patientTab.getPesel(), patientTab.getImie(), patientTab.getNazwisko(), patientTab.getTelefon()));
+            }
+
+            columnTablePeselP.setCellValueFactory(new PropertyValueFactory<>("Pesel"));
+            columnTableNameP.setCellValueFactory(new PropertyValueFactory<>("Imie"));
+            columnTableSurnameP.setCellValueFactory(new PropertyValueFactory<>("Nazwisko"));
+            columnTableEmailP.setCellValueFactory(new PropertyValueFactory<>("Telefon"));
+        } else {
+            alert.setTitle("Uwaga!");
+            alert.setHeaderText("Błąd w polu imie lub nazwisko");
+            alert.setContentText("Podane imie lub naziwsko zawiera błąd lub nie ma go w bazie");
+            alert.showAndWait();
+        }
 
     }
+
     //LIST DOCTOR
 
     //LIST VISIT
