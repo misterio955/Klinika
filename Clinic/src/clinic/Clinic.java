f1123d62e2881666554e36ea6000aab7c27cd3c8
Exatros <exatros@gmail.com>
2018-06-03 13:00:30 +0200

    Obsługa przyciskow logowania i wyjscia. Ukrywanie okna logowania po zalogowaniu

--- Clinic/src/clinic/Clinic.java
+++ Clinic/src/clinic/Clinic.java
@@ -6,6 +6,7 @@
 package clinic;
 
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,19 +14,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Clinic extends Application {
-    
+    static Stage window;
     @Override
-    public void start(Stage Stage) throws IOException {
-       DoctrorWindowController.setIDDoctor(2);
-       Parent root = FXMLLoader.load(getClass().getResource("DoctorWindow.fxml"));
-       Scene scene = new Scene(root);
-       Stage.setScene(scene);
-       //Stage.setTitle("OKNO LOGOWANIA");
-       Stage.show(); 
+    public void start(Stage stage) throws IOException {
+        window = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+        Scene scene = new Scene(root);
+        window.setScene(scene);
+        //Stage.setTitle("OKNO LOGOWANIA");
+        window.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
